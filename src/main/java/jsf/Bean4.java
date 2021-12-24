8c625e035c04e0ddf3d561082bda3a07c15712e6
Saad Moutalib <saad-moutalib@hotmail.fr>
2021-12-24 17:29:12 +0100

    TP TEMPLATES

--- src/main/java/jsf/Bean4.java
+++ src/main/java/jsf/Bean4.java
@@ -28,7 +28,7 @@ public class Bean4 {
     }
     
     public String afficher() {
-        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+        return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
     }
 
     public List<Integer> getNombresSuivants() {
